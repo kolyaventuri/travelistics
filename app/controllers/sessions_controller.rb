e50bccb28c1f2c8b8c6e072cdc128639c28bfc07
Kolya Venturi <kolya.venturi@enx3s.com>
2018-04-01 12:10:31 -0600

    SID is generated

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -10,11 +10,18 @@ class SessionsController < ApplicationController
       flash[:error] = 'Your email or password was incorrect.'
       redirect_to :login
     else
-
+      session[:user_id] = user.id
+      sid = generate_sid
+      session[:sid] = sid
+      cookies[:sid] = sid
+      redirect_to '/register'
     end
   end
 
   private
+  def generate_sid
+    rand(36**64).to_s(36)
+  end
 
   def user_params
     params.require(:user).permit(
