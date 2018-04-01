d1f1f9ca6867eff431ca0facd58bffeb406438ac
Kolya Venturi <kolya.venturi@enx3s.com>
2018-04-01 16:13:01 -0600

    Password change form

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: 'main#index'
 
-  resources :users, only: [:create]
+  resources :users, only: [:create, :update]
   get '/register', to: 'users#new', as: 'registration'
   get '/login', to: 'sessions#login', as: 'login'
 
