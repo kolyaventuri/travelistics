18b881aba066e8df80c6f42782a2bdf9cbc93f90
Kolya Venturi <kolya.venturi@enx3s.com>
2018-04-02 14:09:59 -0600

    Country spec

---
+++ spec/models/country_spec.rb
@@ -0,0 +1,9 @@
+require 'rails_helper'
+
+describe Country, type: :model do
+  it { is_expected.to validate_presence_of(:name) }
+  it { is_expected.to validate_presence_of(:code) }
+  it { is_expected.to validate_presence_of(:side_of_road) }
+
+  it { is_expected.to validate_uniqueness_of(:code) }
+end
