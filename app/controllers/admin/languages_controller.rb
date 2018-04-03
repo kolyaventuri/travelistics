bc577a03cfb4c126f475d9e0726ec36284b86184
Kolya Venturi <kolya.venturi@enx3s.com>
2018-04-03 14:09:03 -0600

    Initial add language to country implementation

--- app/controllers/admin/languages_controller.rb
+++ app/controllers/admin/languages_controller.rb
@@ -8,5 +8,27 @@ module Admin
       @language = Language.new
       @languages = Language.all
     end
+
+    def create
+      country = Country.find(params[:country_id])
+      language = Language.find(language_params[:name])
+
+      if country && country.valid?
+        country.languages << language
+        if country.save
+          redirect_to edit_admin_country_path(country)
+        else
+          flash[:error] = 'Something went wrong'
+          render :new
+        end
+      else
+        redirect_to admin_country_path
+      end
+    end
+
+    private
+    def language_params
+      params.require(:language).permit(:name)
+    end
   end
 end
