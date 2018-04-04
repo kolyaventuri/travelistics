3ecb132c8a97c66920b5283fca32167a1f94e7e4
Kolya Venturi <kolya.venturi@enx3s.com>
2018-04-04 11:46:13 -0600

    Admin can delete language

--- app/controllers/admin/languages_controller.rb
+++ app/controllers/admin/languages_controller.rb
@@ -6,5 +6,11 @@ module Admin
     def index
       @languages = Language.all
     end
+
+    def destroy
+      language = Language.find(params[:id])
+      flash[:error] = 'Could not delete language.' unless language.destroy
+      redirect_to admin_languages_path
+    end
   end
 end
