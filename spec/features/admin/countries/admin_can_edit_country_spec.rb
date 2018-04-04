ee37a71f1f57604fbc676ebb4b883db74ac46be4
Kolya Venturi <kolya.venturi@enx3s.com>
2018-04-04 10:12:45 -0600

    Admin auth checks

--- spec/features/admin/countries/admin_can_edit_country_spec.rb
+++ spec/features/admin/countries/admin_can_edit_country_spec.rb
@@ -1,5 +1,56 @@
 require 'rails_helper'
 
+describe 'Authorization' do
+  before(:all) do
+    DatabaseCleaner.clean
+    @currency = Currency.create!(code: 'USD')
+    @currency2 = Currency.create!(code: 'GBP')
+    @language = Language.create!(name: 'English', code: 'EN')
+    @language2 = Language.create!(name: 'Spanish', code: 'SP')
+
+    @country = Country.create!(name: 'United States', code: 'US', side_of_road: 'right', currency: @currency)
+
+    @country.languages.push(@language)
+  end
+
+  after(:all) do
+    DatabaseCleaner.clean
+  end
+
+  describe 'an admin' do
+    scenario 'can edit the admin country data' do
+      user = User.create!(
+        name: 'Bob Ross',
+        email: 'bob@bobross.com',
+        password: 'happy_little_trees',
+        role: 1
+      )
+
+      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
+
+      visit edit_admin_country_path(@country)
+      expect(page).to have_content(@country.name)
+    end
+  end
+
+  describe 'a regular user' do
+    scenario 'cannot edit admin country data' do
+      user = User.create!(
+        name: 'Bob Ross',
+        email: 'bob@bobross.com',
+        password: 'happy_little_trees',
+        role: 0
+      )
+
+      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
+
+      visit edit_admin_country_path(@country)
+      expect(page).to_not have_content(@country.name)
+      expect(page).to have_content('The page you were looking for doesn\'t exist.')
+    end
+  end
+end
+
 describe 'Admin' do
   before(:all) do
     DatabaseCleaner.clean
