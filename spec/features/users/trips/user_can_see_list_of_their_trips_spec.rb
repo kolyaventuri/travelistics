187dc9490294096f5d0bb996b5924f4e61f8bb34
Kolya Venturi <kolya.venturi@enx3s.com>
2018-04-04 22:21:01 -0600

    Trips endpoint test

---
+++ spec/features/users/trips/user_can_see_list_of_their_trips_spec.rb
@@ -0,0 +1,33 @@
+require 'rails_helper'
+
+describe 'Authorization' do
+  before(:all) do
+    DatabaseCleaner.clean
+    @user = User.create!(
+      name: 'Bob Ross',
+      email: 'bob@bobross.org',
+      password: 'happy_little_trees'
+    )
+  end
+
+  after(:all) do
+    DatabaseCleaner.clean
+  end
+
+  describe 'a logged in user' do
+    scenario 'can access their trips' do
+      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(@user)
+
+      visit user_trips_path
+      expect(page).to have_content('Your Trips')
+    end
+  end
+
+  describe 'a visitor' do
+    scenario 'is redirected to the login page' do
+      visit user_trips_path
+      expect(page).to_not have_content('Your Trips')
+      expect(current_path).to eq(login_path)
+    end
+  end
+end
